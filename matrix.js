618292be467fe20622df35f2e8df1570a4e37d7c
lvnaliyev <53705814+lvnaliyev@users.noreply.github.com>
2021-07-24 22:27:54 +0900

    Auto generating with set interval
    yatto, working

--- matrix.js
+++ matrix.js
@@ -4,15 +4,15 @@ var digit,
     return Math.floor(Math.random() + 0.5);
   },
   codeArea = document.querySelector(".demo"),
-  generate = function (e) {
-    if (e.buttons == "0") {
-      // replace old result with one digit
-      codeArea.innerHTML = randomize();
-      // then generate the rest of the digits
-      for (var i = 1; i < maxDigits; ++i) {
-        digit = randomize();
-        codeArea.innerHTML += digit;
-      }
+  generate = function () {
+    // replace old result with one digit
+    codeArea.innerHTML = randomize();
+    // then generate the rest of the digits
+    for (var i = 1; i < maxDigits; ++i) {
+      digit = randomize();
+      codeArea.innerHTML += digit;
     }
   };
-window.addEventListener("click", generate);
+setInterval(() => generate(), 1);
+
+// e.buttons == "0"
